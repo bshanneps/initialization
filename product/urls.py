aee57e050197095aebdd3c30ba339fe22efdaa9e
bshanneps <bishantneupane@gmail.com>
2019-05-18 13:38:08 +0545

    added to the repositery

--- product/urls.py
+++ product/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('show/', views.show),
     path('product/', views.product),
     path('delete/<int:id>', views.destroy),
-    path('edit/<int:id>', views.change)
+    path('edit/<int:id>', views.edit),
+    path('update/<int:id>', views.update),
+    path('raw_sql/', views.raw_sql),
+    path('api/get_products/', views.get_products, name = "get-products"),
+    path('api/product/', views.ProductView.as_view(), name = 'product'),
+    path('api/product/delete/<int:id>', views.ProductView.as_view(), name = 'product')
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from product.models import Product
 from product.forms import ProductForm
+from django.http import JsonResponse
+from rest_framework.views import APIView
+
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -26,13 +30,45 @@ def product(request):
 def destroy(request, id):
     product = Product.objects.get(id = id)
     product.delete()
-    return redirect("/show")
+    return redirect("/show")    
 
-def change(request, id):
-    if request.method == "GET":
-        form = ProductForm(request.GET)
-        print("happy")
-        product = Product.objects.get(id = id)
-        product.edit()
-    return redirect("/show")
-       
+def edit(request, id):
+    product = Product.objects.get(id = id)
+    return render(request, 'edit.html',{'product' : product})
+
+def update(request, id):
+    product = Product.objects.get(id = id)
+    form = ProductForm(request.POST, instance = product)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'edit.html', {'product': product})
+
+def raw_sql(request):
+    name = ""
+    for p in Product.objects.raw('SELECT * FROM products'):
+        name = name + " " + p.pname
+    return JsonResponse({'result': name})
+
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many = True)
+    return JsonResponse(serializer.data, safe = False)
+
+class ProductView(APIView):
+    def post(self, request):
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'message': 'Product was successful.'})
+        return JsonResponse({'messsage': 'Problem while adding product'})
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    def delete(self, request, id):
+        product = Product.objects.get(id=id)
+        product.delete()
+        return JsonResponse({'message': 'Product Deleted'})  
